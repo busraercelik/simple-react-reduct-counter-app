a46bc0561dc177e5565d9719e1df35503f36103f
Busra Ercelik <bsr.ercelik@gmail.com>
2023-01-13 17:14:09 +0100

    used action payload to change number value

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -4,10 +4,10 @@ const initialState = {number: 0};
 export default function reducer(state = initialState, action) {
     switch (action.type) {
         case 'INCREMENT': {
-            return {...state, number: state.number + 1};
+            return {...state, number: state.number + action.payload};
         }
         case 'DECREMENT': {
-            return  {...state, number: state.number - 1};
+            return  {...state, number: state.number - action.payload};
         }
         default: {
             return state;
